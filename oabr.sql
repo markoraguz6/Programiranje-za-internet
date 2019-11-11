bfe8853a965d0b9981a7b524e0a50408457b1481
markoraguz6 <57632854+markoraguz6@users.noreply.github.com>
2019-11-11 20:53:34 +0100

    Add files via upload

---
+++ oabr.sql
@@ -0,0 +1,232 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 11, 2019 at 03:56 PM
+-- Server version: 10.4.8-MariaDB
+-- PHP Version: 7.1.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `oabr`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `admin_id` varchar(20) NOT NULL,
+  `pwd` varchar(30) DEFAULT NULL,
+  `staff_id` varchar(20) DEFAULT NULL,
+  `name` varchar(20) DEFAULT NULL,
+  `email` varchar(35) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customer`
+--
+
+CREATE TABLE `customer` (
+  `customer_id` varchar(20) NOT NULL,
+  `pwd` varchar(20) DEFAULT NULL,
+  `name` varchar(20) DEFAULT NULL,
+  `email` varchar(35) DEFAULT NULL,
+  `phone_no` varchar(15) DEFAULT NULL,
+  `address` varchar(35) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `flight_details`
+--
+
+CREATE TABLE `flight_details` (
+  `flight_no` varchar(10) NOT NULL,
+  `from_city` varchar(20) CHARACTER SET armscii8 DEFAULT NULL,
+  `to_city` varchar(20) CHARACTER SET armscii8 DEFAULT NULL,
+  `departure_date` date NOT NULL,
+  `arrival_date` date DEFAULT NULL,
+  `departure_time` time DEFAULT NULL,
+  `arrival_time` time DEFAULT NULL,
+  `seats_economy` int(5) DEFAULT NULL,
+  `seats_business` int(5) DEFAULT NULL,
+  `price_economy` int(10) DEFAULT NULL,
+  `price_business` int(10) DEFAULT NULL,
+  `jet_id` varchar(10) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `jet_details`
+--
+
+CREATE TABLE `jet_details` (
+  `jet_id` varchar(10) NOT NULL,
+  `jet_type` varchar(20) CHARACTER SET utf8 DEFAULT NULL,
+  `total_capacity` int(5) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `passengers`
+--
+
+CREATE TABLE `passengers` (
+  `passenger_id` int(10) NOT NULL,
+  `pnr` varchar(15) NOT NULL,
+  `name` varchar(20) DEFAULT NULL,
+  `age` int(3) DEFAULT NULL,
+  `gender` varchar(8) DEFAULT NULL,
+  `meal_choice` varchar(5) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `payment_details`
+--
+
+CREATE TABLE `payment_details` (
+  `payment_id` varchar(20) NOT NULL,
+  `pnr` varchar(15) DEFAULT NULL,
+  `payment_date` date DEFAULT NULL,
+  `payment_amount` int(6) DEFAULT NULL,
+  `payment_mode` varchar(15) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Triggers `payment_details`
+--
+DELIMITER $$
+CREATE TRIGGER `update_ticket_after_payment` AFTER INSERT ON `payment_details` FOR EACH ROW UPDATE ticket_details
+     SET booking_status='CONFIRMED', payment_id= NEW.payment_id
+   WHERE pnr = NEW.pnr
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ticket_details`
+--
+
+CREATE TABLE `ticket_details` (
+  `pnr` varchar(15) NOT NULL,
+  `date_of_reservation` date DEFAULT NULL,
+  `flight_no` varchar(10) DEFAULT NULL,
+  `journey_date` date DEFAULT NULL,
+  `class` varchar(10) DEFAULT NULL,
+  `booking_status` varchar(20) DEFAULT NULL,
+  `no_of_passengers` int(5) DEFAULT NULL,
+  `lounge_access` varchar(5) DEFAULT NULL,
+  `priority_checkin` varchar(5) DEFAULT NULL,
+  `insurance` varchar(5) DEFAULT NULL,
+  `payment_id` varchar(20) DEFAULT NULL,
+  `customer_id` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`admin_id`);
+
+--
+-- Indexes for table `customer`
+--
+ALTER TABLE `customer`
+  ADD PRIMARY KEY (`customer_id`);
+
+--
+-- Indexes for table `flight_details`
+--
+ALTER TABLE `flight_details`
+  ADD PRIMARY KEY (`flight_no`,`departure_date`),
+  ADD KEY `jet_id` (`jet_id`);
+
+--
+-- Indexes for table `jet_details`
+--
+ALTER TABLE `jet_details`
+  ADD PRIMARY KEY (`jet_id`);
+
+--
+-- Indexes for table `passengers`
+--
+ALTER TABLE `passengers`
+  ADD PRIMARY KEY (`passenger_id`,`pnr`),
+  ADD KEY `pnr` (`pnr`);
+
+--
+-- Indexes for table `payment_details`
+--
+ALTER TABLE `payment_details`
+  ADD PRIMARY KEY (`payment_id`),
+  ADD KEY `pnr` (`pnr`);
+
+--
+-- Indexes for table `ticket_details`
+--
+ALTER TABLE `ticket_details`
+  ADD PRIMARY KEY (`pnr`),
+  ADD KEY `customer_id` (`customer_id`),
+  ADD KEY `journey_date` (`journey_date`),
+  ADD KEY `flight_no` (`flight_no`),
+  ADD KEY `flight_no_2` (`flight_no`,`journey_date`);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `flight_details`
+--
+ALTER TABLE `flight_details`
+  ADD CONSTRAINT `flight_details_ibfk_1` FOREIGN KEY (`jet_id`) REFERENCES `jet_details` (`jet_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `passengers`
+--
+ALTER TABLE `passengers`
+  ADD CONSTRAINT `passengers_ibfk_1` FOREIGN KEY (`pnr`) REFERENCES `ticket_details` (`pnr`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `payment_details`
+--
+ALTER TABLE `payment_details`
+  ADD CONSTRAINT `payment_details_ibfk_1` FOREIGN KEY (`pnr`) REFERENCES `ticket_details` (`pnr`) ON UPDATE CASCADE;
+
+--
+-- Constraints for table `ticket_details`
+--
+ALTER TABLE `ticket_details`
+  ADD CONSTRAINT `ticket_details_ibfk_2` FOREIGN KEY (`customer_id`) REFERENCES `customer` (`customer_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `ticket_details_ibfk_3` FOREIGN KEY (`flight_no`,`journey_date`) REFERENCES `flight_details` (`flight_no`, `departure_date`) ON DELETE SET NULL ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
